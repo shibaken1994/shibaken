e0aaab8adfc5bf3a272e17fe90f4bd08062bcc4d
shibaken1994 <62886462+shibaken1994@users.noreply.github.com>
2020-04-03 06:31:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,4 @@
   
-  
 from discord.ext import commands
 import os
 import traceback
@@ -8,11 +7,11 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 @client.event
 async def on_message(message):
@@ -20,5 +19,10 @@ async def on_message(message):
         if message.author.guild_permissions.administrator:
             await message.channel.purge()
             await message.channel.send('ハサキィ！！！')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
 
 bot.run(token)
